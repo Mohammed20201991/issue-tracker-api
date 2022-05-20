e1a7f26c2395478f88e5f723cc75553085b04dc2
Mohammed20201991 <mohamedabid092@gmail.com>
2022-05-20 18:04:27 +0200

    create issue table and seeding with factory

---
+++ app/Http/Controllers/IssueController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Issue;
+use App\Http\Requests\StoreIssueRequest;
+use App\Http\Requests\UpdateIssueRequest;
+
+class IssueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+    //  We do not need to show forms I will delete it
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreIssueRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreIssueRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Issue  $issue
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Issue $issue)
+    {
+        //
+    }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\Issue  $issue
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(Issue $issue)
+    // {
+    //     //
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateIssueRequest  $request
+     * @param  \App\Models\Issue  $issue
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateIssueRequest $request, Issue $issue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Issue  $issue
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Issue $issue)
+    {
+        //
+    }
+}
