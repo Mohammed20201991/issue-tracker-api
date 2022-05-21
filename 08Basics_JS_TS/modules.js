e6d1c6e258cdf6938211183a856aff5e178a68fb
Mohammed20201991 <mohamedabid092@gmail.com>
2022-05-22 01:21:01 +0200

    inti Angular project and add nav and main component

--- 08Basics_JS_TS/modules.js
+++ 08Basics_JS_TS/modules.js
@@ -3,6 +3,6 @@ import dayjs  from "dayjs"
 // ctr + space  to see exported functions from math.js
 console.log(add(10,32));
 console.log(multiply(3,6));
-// you see the function automaticaly imported to the head
+// you see the function automatically imported to the head
 
 console.log(dayjs().format('{YYYY} MM-DDTHH:mm:ss :SSS [Z] A'));
---
+++ issue-tracker-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
